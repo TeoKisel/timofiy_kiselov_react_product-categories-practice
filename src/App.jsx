48a48c7e663fa5cc59dbdfe84e611f17a078f591
Timofei <kiselovtimofej@gmail.com>
2026-09-02 12:04:06 +0300

    Use the input to filter products by name task3

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,18 @@ export const products = productsFromServer.map(product => {
 
 export const App = () => {
   const [selectedOwner, setSelectedOwner] = useState('All');
-  const [query, setQuery] = useState(null);
+  const [query, setQuery] = useState('');
 
   const visibleProducts = products.filter(product => {
-    return selectedOwner === 'All' || product.user.name === selectedOwner;
+    // Find by UserName
+    const matchesOwner =
+      selectedOwner === 'All' || product.user.name === selectedOwner;
+    // Find by input value
+    const matchesQuery = product.name
+      .toLowerCase()
+      .includes(query.toLowerCase());
+
+    return matchesOwner && matchesQuery;
   });
 
   return (
@@ -79,21 +87,25 @@ export const App = () => {
                   type="text"
                   className="input"
                   placeholder="Search"
-                  value="qwe"
+                  value={query}
+                  onChange={event => setQuery(event.target.value)}
                 />
 
                 <span className="icon is-left">
                   <i className="fas fa-search" aria-hidden="true" />
                 </span>
 
-                <span className="icon is-right">
-                  {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                  <button
-                    data-cy="ClearButton"
-                    type="button"
-                    className="delete"
-                  />
-                </span>
+                {query !== '' && (
+                  <span className="icon is-right">
+                    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                    <button
+                      data-cy="ClearButton"
+                      type="button"
+                      className="delete"
+                      onClick={() => setQuery('')}
+                    />
+                  </span>
+                )}
               </p>
             </div>
 
